32de5fb9efabf8ecb67f4dbccdeec5b8e616e4c7
Travis Lavery <travisalavery@gmail.com>
2017-11-24 23:20:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,44 @@
+
+function getFirstSelector(selector) {
+  var match = document.querySelector(selector);
+  return match;
+}
+
+function nestedTarget() {
+  var nested = document.querySelector('#nested');
+  var target = nested.querySelector('.target');
+  return target
+}
+
+function increaseRankBy(n) {
+  var change = parseInt(n);
+  var list = document.querySelectorAll('.ranked-list');
+  for (var i=0; i<list.length; i++) {
+    list[i].innerHTML = (i+change).toString()
+  }
+}
+
+function deepestChild() {
+  var grandNode = document.querySelector('#grand-node');
+  var deepest = '';
+  var current = grandNode.children;
+  var nodeList = [];
+  while(current.length>0) {
+    for (var i=0; i<current.length; i++) {
+      var nextChild = current[i].children
+      
+      if (nextChild) {
+        //console.log(nextChild)
+      //  if(nextChild.children.length>0){
+          for (var j = 0; j<nextChild.length;j++) {
+            console.log(nextChild.children)
+            nodeList.push(nextChild.children[j])
+          }
+       // }
+      }
+    }
+    current = nodeList
+    nodeList = []
+  } 
+  return current
+}
